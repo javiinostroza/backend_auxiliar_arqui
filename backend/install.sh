f640f573bb9496b7304a7458d3c3b62602d7ed3a
JAVIERA INOSTROZA <jminostroza@uc.cl>
2020-11-06 04:38:02 -0300

    Add cicd config

---
+++ backend/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+aws--file-exists-behavior RETAIN
+pwd=$( aws ecr get-login-password )
+docker container stop $(docker container ls -aq)
+docker login -u AWS -p $pwd https://556070822500.dkr.ecr.us-east-2.amazonaws.com
+docker pull 556070822500.dkr.ecr.us-east-2.amazonaws.com/16arqsisv2
